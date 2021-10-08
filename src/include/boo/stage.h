4468715dea11e3ee98b0883a54a37a5f88ca2321
Fruityloops <fruityloops@littun.co>
2021-10-08 12:33:39 +0200

    basic data types for objects and loading them

--- src/include/boo/stage.h
+++ src/include/boo/stage.h
@@ -1,6 +1,7 @@
 #ifndef STAGE_H
 #define STAGE_H
 
+#include <map>
 #include <oead/byml.h>
 #include <oead/sarc.h>
 #include <oead/types.h>
@@ -9,11 +10,50 @@
 namespace boo
 {
 
+    enum StageType
+    {
+        Design, Map, Sound
+    };
+
+    struct UnitConfig
+    {
+        std::string DisplayName;
+        oead::Vector3f DisplayRotate, DisplayScale, DisplayTranslate;
+        std::string GenerateCategory;
+        std::string ParameterConfigName;
+        StageType PlacementTargetFile;
+    };
+
+    struct Object
+    {
+        std::string Id;
+        bool IsLinkDest;
+        std::string LayerConfigName;
+        std::string ModelName;
+        std::string PlacementFileName;
+        oead::Vector3f Rotate, Scale, Translate;
+        UnitConfig UnitConfig;
+        std::string UnitConfigName;
+        std::string comment;
+    };
+
+    struct ObjectList
+    {
+        std::vector<boo::Object> objects;
+    };
+
+    struct StageDataEntry
+    {
+        std::map<std::string, ObjectList> object_lists;
+    };
+
     class StageData
     {
     public:
         u8 Load(oead::Byml& data);
         oead::Byml Save();
+
+        std::vector<StageDataEntry> entries;
     };
 
     class Stage
--- src/stage.cpp
+++ src/stage.cpp
@@ -1,4 +1,5 @@
 #include <boo/stage.h>
+#include <oead/byml.h>
 #include <oead/sarc.h>
 #include <oead/yaz0.h>
 #include <sstream>
@@ -33,13 +34,42 @@ namespace boo
         }
 
         data.Load(stagedata);
-
         return 0;
     }
 
     u8 boo::StageData::Load(oead::Byml &data)
     {
+        u32 i = 0;
+        for (auto entry = data.GetArray().cbegin(); entry != data.GetArray().cend(); ++entry)
+        {
+            entries.push_back(StageDataEntry());
+            for (auto object_list = data.GetArray()[i].GetHash().cbegin(); object_list != data.GetArray()[i].GetHash().cend(); ++object_list)
+            {
+                for (auto object = object_list->second.GetArray().cbegin(); object != object_list->second.GetArray().cend(); ++object)
+                {
+                    Object o;
+                    o.Id = object->GetHash().at("Id").GetString();
+                    o.IsLinkDest = object->GetHash().at("IsLinkDest").GetBool();
+                    o.LayerConfigName = object->GetHash().at("LayerConfigName").GetString();
 
+                    try
+                    {
+                        o.ModelName = object->GetHash().at("ModelName").GetString();
+                    } catch(std::bad_variant_access& e) {o.ModelName = std::string();}
+
+                    o.PlacementFileName = object->GetHash().at("PlacementFileName").GetString();
+                    o.UnitConfigName = object->GetHash().at("UnitConfigName").GetString();
+
+                    try
+                    {
+                        o.comment = object->GetHash().at("comment").GetString();
+                    } catch(std::bad_variant_access& e) {o.comment = std::string();}
+                    
+                    entries[i].object_lists[object_list->first].objects.push_back(o);
+                }
+            }
+            i++;
+        }
         return 0;
     }
 }
