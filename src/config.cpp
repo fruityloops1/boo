fc09d6e685d17057a35dd0866747f4563d47a4df
Fruityloops <fruityloops@littun.co>
2021-10-08 15:53:34 +0200

    spacing

--- src/config.cpp
+++ src/config.cpp
@@ -3,24 +3,24 @@
 namespace boo
 {
 
-u8 boo::Config::Load(std::string file)
-{
-    INIReader reader(file);
-    if (reader.ParseError() != 0) return 1;
-    StageDataPath = reader.Get("config", "StageData", "");
-    ObjectDataPath = reader.Get("config", "ObjectData", "");
-    return 0;
-}
+    u8 boo::Config::Load(std::string file)
+    {
+        INIReader reader(file);
+        if (reader.ParseError() != 0) return 1;
+        StageDataPath = reader.Get("config", "StageData", "");
+        ObjectDataPath = reader.Get("config", "ObjectData", "");
+        return 0;
+    }
 
-u8 boo::Config::Save(std::string file)
-{
-    std::ofstream ini(file);
-    ini << "[config]\n";
-    ini << "StageData=" << StageDataPath << "\n";
-    ini << "ObjectData=" << ObjectDataPath << "";
-    ini.close();
-    if (!ini) return 1;
-    return 0;
-}
+    u8 boo::Config::Save(std::string file)
+    {
+        std::ofstream ini(file);
+        ini << "[config]\n";
+        ini << "StageData=" << StageDataPath << "\n";
+        ini << "ObjectData=" << ObjectDataPath << "";
+        ini.close();
+        if (!ini) return 1;
+        return 0;
+    }
 
 }
