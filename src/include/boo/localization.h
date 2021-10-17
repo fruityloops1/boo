18f8beb9a2508fde833fc38454e96003a8927fa2
Fruityloops <fruityloops@littun.co>
2021-10-17 02:51:40 +0200

    update localization.h

--- src/include/boo/localization.h
+++ src/include/boo/localization.h
@@ -14,10 +14,16 @@ class Localization
 private:
     static const inline std::vector<std::unordered_map<std::string, std::string>> data = {
         { // English
-            {"title", "Boo Editor"}
+            {"title", "Boo Editor"}, {"debug", "Debug"}, {"view", "View"}, {"object_view", "Object view"},
+            {"s_debug", "Debug menu"}, {"s_object_view", "Object view"}, {"select_stage_data", "Select StageData folder"},
+            {"stage_data_notice", "In order to use Boo Editor, you need the StageData folder from your RomFS dump."},
+            {"browse", "Browse"}, {"stage_data_folder", "StageData folder"}, {"not_exist", "Folder does not exist"}, {"ok", "OK"}
         },
         { // German
-            {"title", "Boo Editor"}
+            {"title", "Boo Editor"}, {"debug", "Debug"}, {"view", "Ansicht"}, {"object_view", "Objektansicht"},
+            {"s_debug", "Debug-Menü"}, {"s_object_view", "Objektansicht"}, {"select_stage_data", "StageData Ordner auswählen"},
+            {"stage_data_notice", "Um Boo Editor zu nutzen, benötigst du den StageData Ordner deines RomFS-Dumps."},
+            {"browse", "Durchsuchen"}, {"stage_data_folder", "StageData Ordner"}, {"not_exist", "Der Ordner existiert nicht"}, {"ok", "OK"}
         }
     };
     static inline u8 language;
