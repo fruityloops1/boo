dc9f3a474c883cfaf8a530a926fb04352426f440
Fruityloops <fruityloops@littun.co>
2021-10-21 01:56:53 +0200

    modal bg

--- src/boo.cpp
+++ src/boo.cpp
@@ -83,7 +83,7 @@ int resources()
   	colors[ImGuiCol_NavHighlight]           = ImVec4(1.00f, 0.00f, 0.00f, 1.00f);
   	colors[ImGuiCol_NavWindowingHighlight]  = ImVec4(1.00f, 0.00f, 0.00f, 0.70f);
   	colors[ImGuiCol_NavWindowingDimBg]      = ImVec4(1.00f, 0.00f, 0.00f, 0.20f);
-  	colors[ImGuiCol_ModalWindowDimBg]       = ImVec4(1.00f, 0.00f, 0.00f, 0.35f);
+  	colors[ImGuiCol_ModalWindowDimBg]       = ImVec4(1.00f, 1.00f, 1.00f, 0.25f);
 
   	ImGuiStyle& style = ImGui::GetStyle();
   	style.WindowPadding = ImVec2(8.00f, 8.00f);
